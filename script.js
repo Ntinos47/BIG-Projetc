f705e545e5ec68cda18a31b87b3cdb825e010f01
ntinos <skiadaskonstantinos8@gmail.com>
2024-02-06 11:46:48 +0200

    New Update

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+
+    $('.slick-carousel').slick({
+      prevArrow: '.arrow_prev',
+      nextArrow: '.arrow_next',
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+
+      autoplay: true,
+      autoplaySpeed: 1700,
+      speed: 1600,
+    });
+
+});
