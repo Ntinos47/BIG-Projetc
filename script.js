a42e5fdd99f2a198af9e829774a6e4aa13615466
ntinos <skiadaskonstantinos8@gmail.com>
2024-02-06 13:41:17 +0200

    New upload

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
       prevArrow: '.arrow_prev',
       nextArrow: '.arrow_next',
       infinite: true,
-      slidesToShow: 3,
+      slidesToShow: 4,
       slidesToScroll: 1,
 
       autoplay: true,
-      autoplaySpeed: 1700,
-      speed: 1600,
+      autoplaySpeed: 1900,
+      speed: 1500,
     });
 
 });
